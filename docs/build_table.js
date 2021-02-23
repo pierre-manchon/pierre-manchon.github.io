c2509fbd40966687b2bd466fe8482936f52d57ad
Pierre Manchon <pierre.manchon@pm.me>
2021-02-23 21:27:45 +0100

    Update build_table.js

--- docs/build_table.js
+++ docs/build_table.js
@@ -1,17 +1,14 @@
 let website_links = [
-  {title: "DeepL", link: "https://www.deepl.com/translator"},
-  {title: "FDN", link: "https://www.fdn.fr/services/acces-internet/"},
-  {title: "Sci-Hub", link: "https://sci-hub.now.sh"},
-  {title: "Z Lib", link: "https://z-lib.org/"},
-  {title: "LibGen", link: "http://libgen.rs/"}
+  {title: 'DeepL', link: 'https://www.deepl.com/translator'},
+  {title: 'FDN', link: 'https://www.fdn.fr/services/acces-internet/'},
+  {title: 'Sci-Hub', link: 'https://sci-hub.now.sh'},
+  {title: 'Z Lib', link: 'https://z-lib.org/'},
+  {title: 'LibGen', link: 'http://libgen.rs/'},
+  {title: 'Un monde meilleur', link:'https://abw.blue/index.php'}
 ];
 
 function switchiframe(element) {
-    document.getElementById("main").src=element.name;
-}
-
-function e() {
-  console.log('test');
+    document.getElementById('main').src = element.name;
 }
 
 function generateTable(table, data_array) {
@@ -19,13 +16,13 @@ function generateTable(table, data_array) {
     let row = table.insertRow();
     let txt_cell = row.insertCell();
     let btn_cell = row.insertCell();
-    let txt = document.createTextNode(data['title']);
-    let input = document.createElement("input");
-    input.type = "button";
-    input.name = data["link"];
-    input.value = data["title"];
-    input.onload = e();
-    txt_cell.appendChild(txt);
+    let title = document.createTextNode(data['title']);
+    let input = document.createElement('input');
+    input.type = 'button';
+    input.name = data['link'];
+    input.value = data['title'];
+    input.onload = 'switchiframe(this)';
+    txt_cell.appendChild(title);
     btn_cell.appendChild(input);
   })
 }
